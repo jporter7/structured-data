1f2d98b6a5e28a15e083039a1d59eaa7ead9cf44
jporter7@ycp.edu <jporter7@ycp.edu@kec119-12>
2017-10-21 15:44:37 -0400

    done?

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -90,39 +90,50 @@
      (set (:authors book))))
 
 (defn has-author? [book author]
-  :-)
+  (contains? (:authors book) author))
 
 (defn authors [books]
-  :-)
+  (apply clojure.set/union (map :authors books)))
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [{name :name
+          birth :birth-year
+          death :death-year} author]
+     (str name
+          (if birth
+            (str " (" birth " - " death ")")))))
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (map author->string authors))))
 
 (defn book->string [book]
-  :-)
+  (let [authors (authors->string (get book :authors))
+         title   (get book :title)]
+     (str title ", written by " authors)))
 
 (defn books->string [books]
-  :-)
+  (if (empty? books)
+     (str "No books.")
+     (str (count books) " book" (if (> (count books) 1) "s") ". "
+          (apply str (interpose ". " (map book->string books))) ".")))
+
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [book] (has-author? book author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (first (filter (fn [x] (= name (get x :name))) authors)))
 
 (defn living-authors [authors]
-  :-)
+  (filter alive? authors))
 
 (defn has-a-living-author? [book]
-  :-)
+  (not (empty? (living-authors (get book :authors)))))
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter has-a-living-author? books))
 
 ; %________%
